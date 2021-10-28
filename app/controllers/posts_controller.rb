87bb97d6d1723a23598204df58030e92d9122380
Braulio <b.oyarzun.barros@gmail.com>
2021-10-28 20:54:50 -0300

    added new posts form and show every post in index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,13 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
+  end
+
+  def new_post
   end
 
   def create
+    Post.create(title: params["title"], image_url: params["img_url"], content: params["content"])
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'posts#index'
+  get 'posts/new_post'
   get 'posts/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
